d3002a8a4c21d1c3360a070f1d706ec536f7040b
Edel Ordoñez <ordonez.e.15@hotmail.com>
2022-12-20 18:26:40 -0600

    Added home page

--- pages/Home.js
+++ pages/Home.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {Text} from 'react-native';
-import PropTypes from 'prop-types';
+import {View, StyleSheet, Text} from 'react-native';
+import SearchBar from '../components/SearchBar';
+import Address from '../ui/molecules/Adress';
+import RestaurantsSearch from '../components/RestaurantsSearch';
 
-const Home = props => {
-  return <Text>Home</Text>;
-};
+function Home() {
+  return (
+    <View style={styles.searchContainer}>
+      <SearchBar isHome />
+      <Address />
+      <Text style={styles.title}>Restaurantes Cercanos</Text>
+      <RestaurantsSearch />
+    </View>
+  );
+}
 
-Home.propTypes = {};
+const styles = StyleSheet.create({
+  searchContainer: {
+    backgroundColor: '#fff',
+    padding: 14,
+    paddingHorizontal: 8,
+  },
+  title: {
+    fontWeight: '700',
+    fontSize: 19,
+    paddingLeft: 22,
+    // fontFamily: 'PoppinsSemiBold',
+  },
+});
 
 export default Home;
